bac5a1707109965e0ac41538ff23f0a7c4940e02
yizt <csuyzt@163.com>
2019-10-03 20:58:15 +0800

    命令执行测试

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import sys
 import db
 from config import cur_config as cfg
 import parser
+import run
 
 
 def main(func_id, datatime, mode='normal', business_param=''):
@@ -24,6 +25,11 @@ def main(func_id, datatime, mode='normal', business_param=''):
         cmd_info_list = parser.parse(cmd_cfg_list, session, datatime, business_param)
         session.add_all(cmd_info_list)
         session.commit()
+
+    if mode in ['normal', 'redo']:
+        cmd_todo_list = db.get_exec_cmd(session, datatime, func_id, business_param)
+        cmd_todo_list = [cmd for cmd in cmd_todo_list if cmd.flag is None or cmd.flag != 0]
+        run.run_list(session, cmd_todo_list)
     # 关闭session
     session.close()
 
