eb725561c8f2d3cd6b9e9e54593cca2d4a3f187a
yizt <csuyzt@163.com>
2019-10-03 11:29:08 +0800

    表结构定义

--- tab_ddl.sql
+++ tab_ddl.sql
@@ -1,41 +1,42 @@
+===============================mysql表结构==========================
 
 
 CREATE TABLE `tb_cmd_cfg` (
-  `SEQ` bigint(22) DEFAULT NULL,
-  `FUNC_ID` varchar(40) DEFAULT NULL,
-  `CFG_KEY` varchar(40) DEFAULT NULL,
-  `MEMO` varchar(100) DEFAULT NULL,
-  `EXEC_CMD` longtext,
-  `ENABLE` varchar(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+  `seq` float NOT NULL,
+  `func_id` varchar(40) NOT NULL,
+  `cfg_key` varchar(40) DEFAULT NULL,
+  `memo` varchar(100) DEFAULT NULL,
+  `exec_cmd` longtext,
+  `enable` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`seq`,`func_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `tb_param_cfg` (
-  `FUNC_ID` varchar(20) NOT NULL DEFAULT '',
-  `PARAM_TYPE` varchar(20) NOT NULL DEFAULT '',
-  `PARAM_NAME` varchar(40) NOT NULL DEFAULT '',
-  `PARAM_DESC` varchar(200) DEFAULT NULL,
-  `PARAM_FORMAT` varchar(40) DEFAULT NULL,
-  `PARAM_VAL_EXPR` text,
-  `ENABLE` varchar(1) DEFAULT NULL,
-  `REPLACE_ORDER` bigint(22) DEFAULT NULL,
-  PRIMARY KEY (`FUNC_ID`,`PARAM_TYPE`,`PARAM_NAME`),
-  UNIQUE KEY `IDX_TB_PARAM_CFG` (`FUNC_ID`,`PARAM_NAME`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+  `param_type` varchar(20) NOT NULL DEFAULT '',
+  `param_name` varchar(40) NOT NULL DEFAULT '',
+  `param_desc` varchar(200) DEFAULT NULL,
+  `param_format` varchar(40) DEFAULT NULL,
+  `param_val_expr` text,
+  `enable` varchar(1) DEFAULT NULL,
+  `replace_order` bigint(22) DEFAULT NULL,
+  PRIMARY KEY (`param_type`,`param_name`),
+  UNIQUE KEY `IDX_TB_PARAM_CFG` (`param_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `tb_execcmd` (
-  `DATATIME` varchar(8) NOT NULL DEFAULT '',
-  `FUNC_ID` varchar(40) NOT NULL DEFAULT '',
-  `SEQ` bigint(22) NOT NULL DEFAULT '0',
-  `MEMO` varchar(100) DEFAULT NULL,
-  `EXEC_CMD` longtext,
-  `FLAG` bigint(22) DEFAULT NULL,
-  `ERR_MSG` text,
-  `START_TIME` datetime DEFAULT NULL,
-  `END_TIME` datetime DEFAULT NULL,
-  `EXEC_ELAPSED` bigint(22) DEFAULT NULL,
-  `EXEC_DATE` datetime DEFAULT NULL,
-  `BUSINESS_PARAM` varchar(40) NOT NULL DEFAULT '',
-  PRIMARY KEY (`DATATIME`,`FUNC_ID`,`SEQ`,`BUSINESS_PARAM`)
-) ENGINE
+  `datatime` varchar(8) NOT NULL DEFAULT '',
+  `func_id` varchar(40) NOT NULL DEFAULT '',
+  `seq` bigint(22) NOT NULL DEFAULT '0',
+  `memo` varchar(100) DEFAULT NULL,
+  `exec_cmd` longtext,
+  `flag` smallint(22) DEFAULT NULL,
+  `err_msg` text,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `exec_elapsed` bigint(22) DEFAULT NULL,
+  `exec_date` datetime DEFAULT NULL,
+  `business_param` varchar(40) NOT NULL DEFAULT '',
+  PRIMARY KEY (`datatime`,`func_id`,`seq`,`business_param`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
