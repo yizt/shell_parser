79033e74cb6869321729ea9395bfc43cd63762dd
yizt <csuyzt@163.com>
2019-10-04 15:13:57 +0800

    增加pg下ddl

--- tab_ddl.sql
+++ tab_ddl.sql
@@ -40,3 +40,46 @@ CREATE TABLE `tb_execcmd` (
   `business_param` varchar(40) NOT NULL DEFAULT '',
   PRIMARY KEY (`datatime`,`func_id`,`seq`,`business_param`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+===================postgresql表结构=============
+
+CREATE TABLE public.tb_cmd_cfg (
+	seq float8 NOT NULL,
+	func_id varchar(40) NOT NULL,
+	cfg_key varchar(40) NULL,
+	memo varchar(100) NULL,
+	exec_cmd text NULL,
+	"enable" varchar(1) NULL,
+	CONSTRAINT tb_cmd_cfg_pkey PRIMARY KEY (seq, func_id)
+);
+
+
+CREATE TABLE public.tb_param_cfg (
+	param_type varchar(20) NOT NULL,
+	param_name varchar(40) NOT NULL,
+	param_desc varchar(200) NULL,
+	param_format varchar(40) NULL,
+	param_val_expr text NULL,
+	"enable" varchar(1) NULL,
+	replace_order int8 NULL,
+	CONSTRAINT tb_param_cfg_pkey PRIMARY KEY (param_type, param_name),
+	CONSTRAINT tb_param_cfg_un UNIQUE (param_name)
+);
+
+CREATE TABLE public.tb_execcmd (
+	datatime varchar(8) NOT NULL,
+	func_id varchar(40) NOT NULL,
+	seq int8 NOT NULL,
+	memo varchar(100) NULL,
+	exec_cmd text NULL,
+	flag int2 NULL,
+	err_msg text NULL,
+	start_time timestamp NULL,
+	end_time timestamp NULL,
+	exec_elapsed int8 NULL,
+	exec_date date NULL,
+	business_param varchar(40) NOT NULL,
+	CONSTRAINT tb_execcmd_pkey PRIMARY KEY (datatime, seq, func_id, business_param)
+);
