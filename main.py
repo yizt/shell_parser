0882ff8a53b6a8669a93c5d62da7da394c4f6001
yizt <csuyzt@163.com>
2019-10-04 15:25:45 +0800

    增加备注

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def generate_cmd_list(session, datatime, func_id, business_param):
     """
     生成待执行的命令信息
     :param session:
-    :param datatime:
+    :param datatime: 数据日期
     :param func_id:
-    :param business_param:
+    :param business_param: 自定义的业务参数 eg: a=15,b=abc,c=hello
     :return None:
     """
     cmd_cfg_list = db.get_cmd_cfg(session, func_id)
@@ -30,6 +30,17 @@ def generate_cmd_list(session, datatime, func_id, business_param):
 
 
 def main(func_id, datatime, mode='normal', business_param=''):
+    """
+    主函数
+    :param func_id: 功能id
+    :param datatime: 数据日期
+    :param mode: normal|debug|redo;
+                normal:从上次出错的位置开始执行;如果没有执行过，则从头开始执行
+                redo:删除之前已经执行过的命令，重新生成执行命令，并从头开始执行
+                debug:删除之前已经执行过的命令，重新生成执行命令, 不执行命令
+    :param business_param: 自定义的业务参数 eg: a=15,b=abc,c=hello
+    :return None:
+    """
     session = db.get_session(cfg.url)
     # 命令生成
     if mode in ['debug', 'redo']:
@@ -44,7 +55,7 @@ def main(func_id, datatime, mode='normal', business_param=''):
         # 当前没有生成执行命令,则生成
         if cmd_todo_list is None or len(cmd_todo_list) == 0:
             generate_cmd_list(session, datatime, func_id, business_param)
-            
+
     # 命令执行
     if mode in ['normal', 'redo']:
         cmd_todo_list = db.get_exec_cmd(session, datatime, func_id, business_param)
