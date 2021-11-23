8ccbffd2c14567fa2f14d9449bbd2106a75166b1
yizt <csuyzt@163.com>
2021-11-23 10:06:31 +0800

    样例数据

--- tab_ddl.sql
+++ tab_ddl.sql
@@ -83,3 +83,21 @@ CREATE TABLE public.tb_execcmd (
 	business_param varchar(40) NOT NULL,
 	CONSTRAINT tb_execcmd_pkey PRIMARY KEY (datatime, seq, func_id, business_param)
 );
+
+
+======样例数据======
+INSERT INTO public.tb_param_cfg (param_type,param_name,param_desc,param_format,param_val_expr,"enable",replace_order) VALUES
+('in','$businessparam','业务参数',NULL,NULL,'1',0)
+,('in','$curday','当日','yyyymmdd',NULL,'1',0)
+,('in','$curmon','当月','yyyymm',NULL,'1',0)
+,('single','$nextday','下一日','yyyymmdd','to_char(to_date(''$curday'',''YYYYMMDD'') +  interval ''1 day'',''YYYYMMDD'')','1',1)
+,('set','$mons','测试月份',NULL,'select to_char(now() + (rownum || '' mon'')::interval,''YYYYMM'') from (select row_number() over() as rownum from public.tb_param_cfg) as a where a.rownum<=2','1',1)
+,('set','$number','测试数字',NULL,'select rownum from (select row_number() over() as rownum from public.tb_param_cfg) as a where a.rownum<=2','1',2)
+;
+
+
+INSERT INTO public.tb_cmd_cfg (seq,func_id,cfg_key,memo,exec_cmd,"enable") VALUES
+(2,'test_param',NULL,'测试single参数','$nextday','1')
+,(3.5,'test_param',NULL,'测试set参数','$mons,$number','1')
+,(1,'test_param',NULL,'测试in参数','$curday,$curmon,$a,$b','1')
+;
