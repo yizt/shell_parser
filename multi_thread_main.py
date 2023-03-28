bfd48f507d2ed67a2098f1a5ea5bca157124096a
yizt <csuyzt@163.com>
2023-03-28 09:27:51 +0800

    add:增加多线程解析

--- multi_thread_main.py
+++ multi_thread_main.py
@@ -1,39 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on 2019/10/3 下午5:40
-
-入口类
-
-@author: mick.yi
-
+ @File    : multi_thread_main.py
+ @Time    : 2023/3/17 下午4:28
+ @Author  : yizuotian
+ @Description    : 多线程(进程)并行执行
 """
+import multiprocessing
 import sys
 
 import db
-import parser
 import run
 from config import cur_config as cfg
+from main import generate_cmd_list
 
 
-def generate_cmd_list(session, datatime, func_id, business_param, runtime_dict=None):
-    """
-    生成待执行的命令信息
-    :param session:
-    :param datatime: 数据日期
-    :param func_id:
-    :param business_param: 自定义的业务参数 eg: a=15,b=abc,c=hello
-    :param runtime_dict:
-    :return None:
-    """
-    cmd_cfg_list = db.get_cmd_cfg(session, func_id)
-    cmd_info_list = parser.parse(cmd_cfg_list, session, datatime, business_param, runtime_dict)
-    session.add_all(cmd_info_list)
-    session.commit()
+def process_run(pno, cmd_todo_list):
+    session = db.get_session(cfg.url)
+    run.run_list(session, cmd_todo_list)
+    session.close()
 
 
-def main(func_id, datatime, mode='normal', business_param=''):
+def multi_thread_run(num_processes, func_id, datatime, mode='normal', business_param=''):
     """
     主函数
+    :param num_processes:线程数
     :param func_id: 功能id
     :param datatime: 数据日期
     :param mode: normal|debug|redo;
@@ -44,28 +34,39 @@ def main(func_id, datatime, mode='normal', business_param=''):
     :return None:
     """
     session = db.get_session(cfg.url)
+    runtime_dict = {"$num_processes": num_processes}
     # 命令生成
     if mode in ['debug', 'redo']:
         # 先删除已存在的执行信息
         db.delete_exec_cmd(session, datatime, func_id, business_param)
         session.commit()
         # 然后重新生成
-        generate_cmd_list(session, datatime, func_id, business_param)
+        generate_cmd_list(session, datatime, func_id, business_param, runtime_dict)
 
     if mode == 'normal':
         cmd_todo_list = db.get_exec_cmd(session, datatime, func_id, business_param)
         # 当前没有生成执行命令,则生成
         if cmd_todo_list is None or len(cmd_todo_list) == 0:
-            generate_cmd_list(session, datatime, func_id, business_param)
+            generate_cmd_list(session, datatime, func_id, business_param, runtime_dict)
 
     # 命令执行
     if mode in ['normal', 'redo']:
         cmd_todo_list = db.get_exec_cmd(session, datatime, func_id, business_param)
         cmd_todo_list = [cmd for cmd in cmd_todo_list if cmd.flag is None or cmd.flag != 0]
-        run.run_list(session, cmd_todo_list)
-    # 关闭session
-    session.close()
+        # 关闭session
+        session.close()
+        # run.run_list(session, cmd_todo_list)
+        processes = []
+        num_processes = int(num_processes)
+        for i in range(num_processes):
+            cur_cmd_list = [cmd for cmd in cmd_todo_list if cmd.seq % num_processes == i]
+            p = multiprocessing.Process(target=process_run, args=(i, cur_cmd_list))
+            p.start()
+            processes.append(p)
+        # 等待执行完成
+        for p in processes:
+            p.join()
 
 
 if __name__ == '__main__':
-    main(*sys.argv[1:])
+    multi_thread_run(*sys.argv[1:])
